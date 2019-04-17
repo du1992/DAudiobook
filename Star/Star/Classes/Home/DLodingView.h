a3d408378f23966c2a1b29b11fe80ca5af9d0e5d
DUCHENGWEN <kevindcw@163.com>
2019-04-17 15:52:05 +0800

    版本2.0初始化

--- Star/Star/Classes/Home/DLodingView.h
+++
@@ -1,9 +0,0 @@
-//  Created by DUCHENGWEN on 2016/12/3.
-//  Copyright © 2016年 beijing. All rights reserved.
-
-#import <UIKit/UIKit.h>
-
-@interface DLodingView : UIView
-@property (nonatomic,weak) UIActivityIndicatorView *indicatorView;
-@property (nonatomic,weak) UILabel *hintLabel;
-@end
--- Star/Star/Classes/Home/Tool/DHeaderScrollTool.h
+++
@@ -1,9 +0,0 @@
-//
-//  Created by DUCHENGWEN on 2016/12/3.
-//  Copyright © 2016年 beijing. All rights reserved.
-
-#import <Foundation/Foundation.h>
-
-@interface DHeaderScrollTool : NSObject
-+(NSArray *)headerPhotos:(NSString *)htmlString;
-@end
